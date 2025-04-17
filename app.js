ff30f1efbe598143681eb6f55df1bf5b706c7ef8
abdullahi samsudeen <abdullahisamsudeen@gmail.com>
2025-04-17 16:43:49 +0100

    commit

--- app.js
+++ app.js
@@ -28,9 +28,6 @@ userModel = require ('./models/user.model')
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUD_NAME, 
-
-
-
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_SECRET 
   });
